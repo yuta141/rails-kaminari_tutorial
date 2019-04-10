9c54818dc65d24223cdd93e1e7e3f7d28df994d5
yuta141 <yuta.endofeternity@gmail.com>
2019-04-10 19:00:01 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  resources :shops
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+1000.times do |i|
+    Shop.create!(
+      name:    "店名#{i}",
+      zipcode: "111-#{sprintf("%04d", i)}",
+      address: "住所#{i}",
+      tel:     "00-1234-#{sprintf("%04d", i)}"
+    )
+  end
